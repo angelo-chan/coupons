709c7a89544448db63d364e769f61d777abc1528
Angelo Chan <chriswinniechen@gmail.com>
2017-03-03 20:23:17 +0800

    enhance readme

--- coupons-core/src/main/java/com/chainz/coupon/core/repository/CouponRepository.java
+++ coupons-core/src/main/java/com/chainz/coupon/core/repository/CouponRepository.java
@@ -2,6 +2,10 @@ package com.chainz.coupon.core.repository;
 
 import com.chainz.coupon.core.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-/** Coupon repository. */
-public interface CouponRepository extends JpaRepository<Coupon, Long> {}
+/**
+ * Coupon repository.
+ */
+public interface CouponRepository extends JpaRepository<Coupon, Long>, QueryDslPredicateExecutor<Coupon> {
+}
