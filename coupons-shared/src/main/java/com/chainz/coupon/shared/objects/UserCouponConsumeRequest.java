165845b37cc852e842f7611fcce3fe548cfb4924
Angelo Chan <chriswinniechen@gmail.com>
2017-03-12 18:23:30 +0800

    remove unused import

--- coupons-shared/src/main/java/com/chainz/coupon/shared/objects/UserCouponConsumeRequest.java
+++ coupons-shared/src/main/java/com/chainz/coupon/shared/objects/UserCouponConsumeRequest.java
@@ -3,7 +3,6 @@ package com.chainz.coupon.shared.objects;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
